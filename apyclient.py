a8a7291ecb4220245aaa99f08b464c3537c643e8
Aaron Madison <aaron.l.madison@gmail.com>
2016-11-01 15:33:59 -0500

    Flake8: Flake 8 cleanup

--- apyclient.py
+++ apyclient.py
@@ -5,18 +5,12 @@
 # Released subject to the BSD License
 
 
-__all__ = (
-    'api_request',
-    'SignedAPIRequest',
-    'BaseResponse',
-    'JSONApiResponse',
-)
-
-
 from functools import wraps
 import json
 import socket
 import six
+
+import apysigner
 
 if six.PY2:
     from urllib import urlencode
@@ -28,7 +22,12 @@ else:
     from urllib.error import HTTPError
 
 
-import apysigner
+__all__ = (
+    'api_request',
+    'SignedAPIRequest',
+    'BaseResponse',
+    'JSONApiResponse',
+)
 
 
 class BaseResponse(object):
@@ -140,7 +139,6 @@ class APIRequest(object):
         else:
             return response
 api_request = APIRequest
-
 
 
 class SignedURLMixin(object):
